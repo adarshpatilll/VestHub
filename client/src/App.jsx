aa753003eb3d6b3ce380c6baed6c67079f09313c
Adarsh Patil <adarsh.patil81359@outlook.com>
2025-08-27 15:13:31 +0530

    refactor: remove unused imports from App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,6 @@
 import ResponsiveToaster from "./components/ResponsiveToaster";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router.config";
-import { useFundsContext } from "./context/FundContext";
-import { useEffect } from "react";
 
 const App = () => {
    return (
